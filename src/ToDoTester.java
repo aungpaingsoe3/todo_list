850c513cbec66c6a6e2ea6b7c1a28ea3a071252a
Aung Paing Soe <aungpaingsoe03@gmail.com>
2023-06-24 11:21:29 -0700

    Started the To-Do List Application Project

--- src/ToDoTester.java
+++ src/ToDoTester.java
@@ -1,7 +1,7 @@
 public class ToDoTester {
     public static void main(String[] args) {
-//        EventsToDo test = new EventsToDo("test", "3/5/2023");
-//        System.out.println(test.getEventName() + " " + test.getDue() + " " + test.isDone());
+        EventsToDo test = new EventsToDo("test", "3/5/2023");
+        System.out.println(test.getEventName() + " " + test.getDue() + " " + test.isDone());
 
     }
 }
