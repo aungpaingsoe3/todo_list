5eb5168af83722019182a916ede9e2427debc02e
Aung Paing Soe <aungpaingsoe03@gmail.com>
2023-06-25 10:03:51 -0700

    Tested how to close a frame while opening another one

---
+++ src/ToDoGUI.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+import javax.swing.event.*;
+
+public class ToDoGUI {
+    public ToDoGUI(){
+        JFrame testFrame = new JFrame();
+        testFrame.setSize(200,200);
+        JButton testButton = new JButton("Click here");
+        testButton.setSize(50,50);
+        testButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                testFrame.setVisible(false);
+                JFrame newFrame = new JFrame();
+                newFrame.setSize(200,200);
+                JTextField aField = new JTextField("6/25/2023 - SUNDAY");
+                aField.setEditable(false);
+                newFrame.add(aField);
+                newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                newFrame.pack();
+                newFrame.setVisible(true);
+            }
+        });
+        testFrame.setLayout(new BorderLayout());
+        testFrame.add(testButton, BorderLayout.CENTER);
+        testFrame.pack();
+        testFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        testFrame.setVisible(true);
+    }
+}
--- src/ToDoTester.java
+++ src/ToDoTester.java
@@ -2,6 +2,6 @@ public class ToDoTester {
     public static void main(String[] args) {
         EventsToDo test = new EventsToDo("test", "3/5/2023");
         System.out.println(test.getEventName() + " " + test.getDue() + " " + test.isDone());
-
+        ToDoGUI gui = new ToDoGUI();
     }
 }
